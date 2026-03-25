c151204a504a901f692444b94aa752efb489c053
Re-Panza <111627325+Re-Panza@users.noreply.github.com>
2026-03-25 13:39:10 +0100

    Bump version to 102.0 and update changelog
    Updated APP_VERSION to 102.0 and modified changelog.

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-var APP_VERSION = 101.0;
+var APP_VERSION = 102.0;
 
 var APP_CHANGELOG = `
-  <li>nuova funzione storico castelli, adesso vedremo le conquiste, le perdite di tutti i castelli</li>`;
+  <li></li>`;
